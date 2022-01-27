49f0e77361543a4ed45ed52585a2fe7f3b18198c
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-27 12:08:47 -0500

    commit tree b86ed030429b3f3bba1a6bdcfe5ab90ad5f3c05a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, eventName) {
+    const newArray = []
+    for (let i=0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number--)
+    }
+}
